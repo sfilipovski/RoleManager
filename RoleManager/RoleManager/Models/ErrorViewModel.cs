ee133b3c6568ee8fe56aecc3085f226cba66460d
sfilipovski <sergejfilipovski@gmail.com>
2023-08-07 02:33:22 +0200

    Initial project

---
+++ RoleManager/RoleManager/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RoleManager.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
